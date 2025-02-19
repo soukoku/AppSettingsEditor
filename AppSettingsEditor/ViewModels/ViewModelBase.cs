a0376a83b24e61c3733c94e9523a04f1cbf1dead
Eugene Wang <8755753+soukoku@users.noreply.github.com>
2025-02-18 22:10:20 -0500

    Initial avalonia project.

---
+++ AppSettingsEditor/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AppSettingsEditor.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ AppSettingsEditor/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AppSettingsEditor.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
